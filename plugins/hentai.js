a2665410b1985086ff7dea5fe13404645c0bace2
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-01-20 15:29:46 +0700

    Add files via upload

---
+++ plugins/hentai.js
@@ -0,0 +1,25 @@
+let hmtai = require('hmtai')
+
+let handler = async(m, { conn }) => {
+
+let img = await hmtai.nsfw.hentai()
+await conn.sendFile(m.chat, img, '', '', m)
+
+}
+handler.help = ['hentai']
+handler.tags = ['dewasa']
+
+handler.command = /^(hentai)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.limit = true
+
+module.exports = handler
