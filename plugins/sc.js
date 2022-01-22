f73ab77fbf3acab0fd60b1256f0962494542ed3d
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-01-22 15:26:46 +0700

    Delete sc.js

--- plugins/sc.js
+++
@@ -1,9 +0,0 @@
-let handler = async m => m.reply(`
-Bot Ini Mengunakan Sc : 
-https://github.com/Johannes2803/Mythia-Batford
-`.trim()) // Tambah sendiri kalo mau
-handler.help = ['sc']
-handler.tags = ['info']
-handler.command = /^sc$/i
-
-module.exports = handler
