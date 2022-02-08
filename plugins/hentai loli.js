ed3113553783356c25516715c7f11e251f878701
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-02-08 15:44:56 +0700

    Update hentai loli.js

--- plugins/hentai loli.js
+++ plugins/hentai loli.js
@@ -14,28 +14,7 @@ async function getRandom(url) {
     
     handler.command = /^(hentailoli)$/i
     
-    handler.owner = false
-    
-    handler.mods = false
-    
-    handler.premium = false
-    
-    handler.group = false
-    
-    handler.private = false
-    
-    
-    
-    handler.admin = false
-    
-    handler.botAdmin = false
-    
-    
-    
-    handler.fail = null
-    
-    handler.limit = 1
-    
-    
+    handler.premium = true
+    handler.register = true
     
     module.exports = handler
