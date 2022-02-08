2770cd4c60f2c9734b143aef55c533f7274fdb83
Luigmntng <97533143+Luigmntng@users.noreply.github.com>
2022-02-08 15:43:51 +0700

    Update hentai.js

--- plugins/hentai.js
+++ plugins/hentai.js
@@ -9,16 +9,8 @@ handler.help = ['hentai']
 handler.tags = ['dewasa']
 
 handler.command = /^(hentai)$/i
-handler.owner = false
 
-handler.mods = false
-handler.premium = true
-handler.group = false
-handler.private = false
-handler.admin = false
-handler.botAdmin = false
-handler.fail = null
-handler.limit = true
-
+handler.premium = true 
+handler.register = true
 
 module.exports = handler
